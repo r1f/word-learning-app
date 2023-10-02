1f5d2d15bbf78f82f3400b904636bf9d289f3c30
r1f <r1ffourll@gmail.com>
2023-10-02 19:45:38 +0300

    added a dictionary create button and its behavior

--- renderer/js/sidebar.js
+++ renderer/js/sidebar.js
@@ -4,6 +4,13 @@ const toggle = document.querySelector(".toggle");
 const modeSwitch = document.querySelector(".toggle-switch");
 const modeText = document.querySelector(".mode-text");
 
+
+const fs = require('fs');
+const path = require('path');
+
+const dictionaryPath = "./dictionaries/";
+
+
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
@@ -16,26 +23,79 @@ modeSwitch.addEventListener("click", () => {
     : "Dark Mode";
 });
 
-//let dictName = document.querySelector(".dictionary-name");
-//dictName.innerText = files[0];
 
-const fs = require('fs');
-const files = fs.readdirSync('./dictionaries');
+document.querySelector(".popup-add-dict .popup-close-btn").addEventListener("click", function(){
+  document.querySelector(".popup-add-dict").classList.remove("active")
+})
 
-function addDictionary(files){
-  const box = document.getElementsByClassName("box-items");
+document.querySelector(".popup-add-dict .popup-add-btn").addEventListener("click", function(){
+  document.querySelector(".popup-add-dict").classList.remove("active")
+})
 
-  for (file in files)
-  {
+function getDictionaryName(){
+  document.querySelector(".popup-add-dict").classList.add("active");
+}
+
+
+function getSorted(){
+  directoryContent = fs.readdirSync(dictionaryPath);
+
+  let files = directoryContent.filter((filename) => {
+      return fs.statSync(`${dictionaryPath}/${filename}`).isFile();
+  });
+
+  let sorted = files.sort((a, b) => {
+      let aStat = fs.statSync(`${dictionaryPath}/${a}`),
+          bStat = fs.statSync(`${dictionaryPath}/${b}`);
+      
+      return new Date(bStat.birthtime).getTime() - new Date(aStat.birthtime).getTime();
+  });
+
+  return sorted;
+}
+
+
+function loadDictionary(){
+  const files = getSorted();
+  const box = document.getElementsByClassName("cards");
+
+  for (file in files){
     const dictionaryItem = document.createElement("div");
-    dictionaryItem.className = 'box-item';
+    
+    dictionaryItem.className = 'card';
+    
     dictionaryItem.innerHTML = `
-      <div class="box-item">
-      <h1 class='dictionary-name'>${files[file]}</h1>
-      <p><button>Add</button></p>
+      <div class="card-name">
+      <p>${files[file]}</p>
+      </div>
+      <div class="card-btns">
+      <button class="button">Edit</button>
+      <button class="button">Learn</button>
       </div>`;
+
     box[0].appendChild(dictionaryItem);
   }
 }
 
-addDictionary(files);
+loadDictionary();
+
+function createDictionary()
+{
+  let dictName = document.querySelector("#dict-name").value;
+
+  fs.writeFile(dictionaryPath + dictName + ".md", '', function (err) {
+    if (err) throw err;
+  });
+
+  reloadDictionary();
+}
+
+function reloadDictionary(){
+  const box = document.getElementsByClassName("card");
+
+  while (box.length > 1) {
+    box[1].remove();
+  }
+
+  loadDictionary();
+}
